e988e9ccc4e7c9c2886ba6d29be3bd59ce1c2c01
Denys Materna <denys.materna@gmail.com>
2021-09-27 19:29:16 +0300

    test commit

--- src/Layout/AppNavBar/NavBar.js
+++ src/Layout/AppNavBar/NavBar.js
@@ -1,10 +1,7 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment} from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 import Container from "react-bootstrap/Container";
-import { ProjectImages } from "../../components/index";
-import logo1 from "../../assets/images/killer-whale.svg";
 
 const AppNavBar = () => {
   return (
