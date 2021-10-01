01744ce4cf755c35f8ad760f393c9fd7ffe429ce
Denys Materna <denys.materna@gmail.com>
2021-10-01 18:51:06 +0300

    cosmetic changes

--- src/components/main components/SinglePage.js
+++ src/components/main components/SinglePage.js
@@ -23,16 +23,18 @@ export default function SinglePage({ match }) {
         <h2>LOADING</h2>
       ) : (
         <div className="single-show__page text-light">
-          <img
-            src={
-              singleShow.image
-                ? singleShow.image.medium
-                : 'http://mtd.lg.ua/img/articles/NoImageAvailable.png'
-            }
-            alt={singleShow.name}
-          />
+          <div className="mr-5">
+            <img
+              src={
+                singleShow.image
+                  ? singleShow.image.medium
+                  : 'http://mtd.lg.ua/img/articles/NoImageAvailable.png'
+              }
+              alt={singleShow.name}
+            />
+          </div>
           <div className="single-show__info">
-            <h1>{singleShow.name}</h1>
+            <h1 className="single-show__title">{singleShow.name}</h1>
             {singleShow.genres &&
               singleShow.genres.map((genre) => (
                 <span key={genre} className="single-show__info__genre">
@@ -56,6 +58,7 @@ export default function SinglePage({ match }) {
                   href={singleShow.officialSite}
                   target="_blank"
                   rel="noreferrer"
+                  className="single-show__site text-light"
                 >
                   {singleShow.officialSite}
                 </a>
@@ -63,7 +66,9 @@ export default function SinglePage({ match }) {
                 'No site available'
               )}
             </p>
-            <p>{singleShow.summary && removeTags(singleShow.summary)}</p>
+            <p className="single-show__summary bg-light text-dark">
+              {singleShow.summary && removeTags(singleShow.summary)}
+            </p>
           </div>
         </div>
       )}
