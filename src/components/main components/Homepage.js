f97465bfd7dbedc556901c319ac29b3abd0c83cd
Denys Materna <denys.materna@gmail.com>
2021-10-01 20:08:33 +0300

    last version

--- src/components/main components/Homepage.js
+++ src/components/main components/Homepage.js
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div className="homepage text-light text-center">
       <div className="homepage__content pt-2">
         <h1 className="homepage__title ">CONSFILM</h1>
-        <p className="font-weight-bold">Your personal assistant</p>
+        <p className="font-weight-bold">Your personal show assistant</p>
         <p className="font-weight-bold">Are you ready? Then let's start!</p>
         <div className="pt-2">
         <Link to="/login" className="login-link">Log In</Link>
